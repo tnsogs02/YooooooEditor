05ff54af66e33acb17354e5eefd3cfaf54cfdc70
henry.li.chiu.heng@icloud.com <henry.li.chiu.heng@icloud.com>
2020-11-13 11:10:44 +0800

    Add colorfulness func

---
+++ imgconv.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+#include <opencv2/imgproc.hpp>
+
+Mat Colorfulness(Mat img, int i);	//int i from -100 to 100
