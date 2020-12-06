ffd0e779fc0a9fa030ee2e8b88c53920511bf408
曜劑師 東京 <firestar00101@hotmail.com>
2020-12-06 14:13:33 +0800

    123

--- img_cont_bri.cpp
+++ img_cont_bri.cpp
@@ -2,8 +2,10 @@
 
 cv::Mat adjust_contrast(cv::Mat image, int contrast)
 {
+	cv::Mat newimage;
+	image.copyTo(newimage);
 	cv::Mat imageRGB[3];
-	cv::split(image, imageRGB);
+	cv::split(newimage, imageRGB);
 	double _contrast = contrast;
 	double contrast_factor = 1 + _contrast / 100;
 	double aver = (mean(imageRGB[0])[0] + mean(imageRGB[1])[0] + mean(imageRGB[2])[0]) / 3;
@@ -11,9 +13,9 @@ cv::Mat adjust_contrast(cv::Mat image, int contrast)
 	imageRGB[1] = contrast_factor * (imageRGB[1] - aver) + aver;
 	imageRGB[2] = contrast_factor * (imageRGB[2] - aver) + aver;
 
-	merge(imageRGB, 3, image);
+	merge(imageRGB, 3, newimage);
 
-	return image;
+	return newimage;
 }
 
 cv::Mat adjust_brightness(cv::Mat image, int brightness)
@@ -23,30 +25,32 @@ cv::Mat adjust_brightness(cv::Mat image, int brightness)
 
 cv::Mat adjust_brightness(cv::Mat image, int brightness, double arguments)
 {
+	cv::Mat newimage;
+	image.copyTo(newimage);
 	cv::Mat imageRGB[3];
-	cv::split(image, imageRGB);
+	cv::split(newimage, imageRGB);
 	if (arguments < 1)arguments = 1;
 	double _arguments = arguments, _brightness = brightness;
 	double aver = (mean(imageRGB[0])[0] + mean(imageRGB[1])[0] + mean(imageRGB[2])[0]) / 3.0;
 	aver *= _arguments;
 	double x;
-	for (int i = 0; i < image.rows; i++)
+	for (int i = 0; i < newimage.rows; i++)
 	{
-		for (int j = 0; j < image.cols; j++)
+		for (int j = 0; j < newimage.cols; j++)
 		{
 			x = 0;
 			for (int k = 0; k < 3; k++)
 			{
-				x += image.at<cv::Vec3b>(i, j)[k];
+				x += newimage.at<cv::Vec3b>(i, j)[k];
 			}
 			if (x >= aver)
 			{
 				for (int k = 0; k < 3; k++)
 				{
-					image.at<cv::Vec3b>(i, j)[k] = cv::saturate_cast<uchar>((image.at<cv::Vec3b>(i, j)[k]) + _brightness);
+					newimage.at<cv::Vec3b>(i, j)[k] = cv::saturate_cast<uchar>((newimage.at<cv::Vec3b>(i, j)[k]) + _brightness);
 				}
 			}
 		}
 	}
-	return image;
+	return newimage;
 }
