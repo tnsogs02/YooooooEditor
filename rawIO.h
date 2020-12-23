c2b156525f8f9b8e6703af841e8a6ca73f95946d
櫻野はれた <andy19991214@gmail.com>
2020-12-23 23:14:33 +0800

    rawIO with func variables type modified

--- rawIO.h
+++ rawIO.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <iostream>
+#include <iomanip>
+#include <cassert>
+
+#include "exiv2/exiv2.hpp"
+#include "libraw/libraw.h"
 
 #define YORECORDNAME "Iptc.0x0019.0x0000"
 
-class __declspec(dllimport) rawIO {
+extern "C" class __declspec(dllimport) rawIO {
 public:
-	int tiffExport(char* inPath, char* outPath);
-	int iptcWrite(std::string filePath, std::string& iptcValue);
-	int iptcRead(std::string filePath, std::string& iptcValue);
-private:
+	int _stdcall tiffExport(char* inPath, char* outPath);
+	int _stdcall iptcWrite(char* filePath, char* iptcValue);
+	int _stdcall iptcRead(char* filePath, char* iptcValue);
+	int _stdcall exifRead(char* inPath, char* exifValue);
 };
