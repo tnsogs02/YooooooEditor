2f3e31b293095ab160793d2b8132d4b4b89e4d8f
henry.li.chiu.heng@icloud.com <henry.li.chiu.heng@icloud.com>
2020-12-25 21:55:57 +0800

    Update Algo. And merge other's code into Header.h and Source.cpp

--- imgconv.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <iostream>
-
-#include <opencv2/imgproc.hpp>
-
-Mat Colorfulness(Mat img, int i);	//int i from -100 to 100
